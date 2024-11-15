f88f3cd16a7017eb2968ed05b8d21f88e29cce3c
HaiPDV <141106097+hainqm@users.noreply.github.com>
2024-11-15 15:12:42 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    include "./view/header.php";
+    if(isset($_GET['act']) && ($_GET['act']!="")){
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'home':
+                include "./view/home.php";
+                break;
+            case 'product':
+                include "./view/product.php";
+                break;
+            case 'contact':
+                include "./view/contact.php"; 
+                break;
+            case 'about':
+                include "./view/about.php"; 
+                break;
+            case 'detail':
+                include "./view/detail.php"; 
+                break;
+            default:
+                include "./view/home.php";
+                break;
+        }
+    }else{
+        include "./view/home.php";
+    }
+    include "./view/footer.php";
+?>
---
+++ view/detail.php
@@ -0,0 +1,37 @@
+<div class="container">
+    <div class="card mb-3" style="max-width: 1200px; max-height:700px;">
+    <div class="row g-0">
+        <div class="col-md-5">
+        <img src="view/imgs/sp1" class="img_detail img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-7">
+        <div class="card-body">
+            <h3 class="card-title">CASIO G-SHOCK MRG-B2100D-1A</h3>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            
+        </div>
+        <div class="size">
+            <button type="button" class="btn btn-outline-dark">39cm</button>
+            <button type="button" class="btn btn-outline-dark">39cm</button>
+            <button type="button" class="btn btn-outline-dark">39cm</button>
+        </div>
+        <div class="price">
+            <h3>35.000.000</h3>
+        </div>
+        <div class="original_price">
+            <h4><s>45.000.000</s></h4>
+        </div>
+
+        <div class="button">
+            <button type="button" class="btn btn-dark">Buy now</button>
+            <button type="button" class="btn btn-outline-dark">Add cart</button>
+        </div>
+        </div>
+    </div>
+    </div>
+    
+    
+</div>
+
+
+
